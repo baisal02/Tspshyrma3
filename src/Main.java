3ce1f2c2c8d1a49ed310eebfb99bf7c2c88b9e48
Lenovo <altynbekuulubajsal@gmail.com>
2022-08-11 14:48:26 +0300

    jbdfvjhd

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,16 @@ public class Main {
         System.out.println("==");
         System.out.println("==");
         System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
+        System.out.println("==");
     }
 }
